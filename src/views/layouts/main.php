7bb82114c342c63deee3cb31d8f588c9a289f243
shs1 <vitaliy.k@outsource-to-ukraine.com>
2018-04-06 18:09:53 +0300

    change layout

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -108,9 +108,32 @@ $lastActive = \bizley\podium\models\Activity::lastActive();
 </div>
 
 <footer class="eh_footer">
+    <div class="text-footer text-center">
+        <p><?= Yii::t('common', 'Evolution Health is not a healthcare provider and does not provide medical advice, diagnosis, or treatment.
+            If you are currently thinking about or planning to harm yourself or someone else please call 911 or go to
+            the nearest hospital emergency room.') ?> </p>
+    </div>
+    <div class="text-footer text-center">
+        <div class="row">
+            <div class="col-sm-3">
+                <?= HTML::a( Yii::t('common','CONTACT'),'http://www.evolutionhs.com/contact.html',["target"=>"_blank"])?>
+            </div>
+            <div class="col-sm-3">
+                <?= Yii::t('common','ABOUT')?>
+            </div>
+            <div class="col-sm-3">
+                <?= Yii::t('common','THERMS OF USE')?>
+            </div>
+            <div class="col-sm-3">
+                <?= Yii::t('common','PRIVACY')?>
+            </div>
+        </div>
+    </div>
     <div class="container text-center">
-        <p>© Copyright <?php echo date('Y') ?> <a  href="http://www.evolutionhs.com" target="_blank">Evolution Health Systems</a>.
-            All Rights Reserved.</p>
+        <p>© <?= Yii::t('common', 'Copyright');
+            echo date('Y') ?> <a href="http://www.evolutionhs.com" target="_blank">Evolution Health
+                Systems</a>.
+            <?= Yii::t('common','All Rights Reserved.')?>All Rights Reserved.</p>
     </div>
 </footer>
 <?php $this->endBody() ?>
