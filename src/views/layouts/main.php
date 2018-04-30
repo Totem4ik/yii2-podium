69f06123dfa080e256aee81ab26c330563006672
shs1 <vitaliy.k@outsource-to-ukraine.com>
2018-04-30 13:25:15 +0300

    change layouts

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -11,30 +11,139 @@ use bizley\podium\assets\PodiumAsset;
 use bizley\podium\helpers\Helper;
 use bizley\podium\widgets\Alert;
 use yii\helpers\Html;
+use yii\helpers\Url;
+use app\models\Client;
+use app\models\Font;
+use app\models\Theme;
+use app\components\CheckAccessClient;
 
 PodiumAsset::register($this);
-$this->beginPage() ?>
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => Url::to(["/uploads/favicon.ico"])]);
+$this->beginPage();
+$this->title = 'Are You Struggling with Depression or Anxiety?';
+
+if (isset($_SESSION['clientId'])) {
+    $model=Theme::find()->select('font_id')->where(['client_id'=>$_SESSION['clientId']])->one();
+    $font=Font::findOne($model->font_id);
+}
+else{
+    $accessSite = new CheckAccessClient($_SERVER['HTTP_HOST']);
+    $accessSite->checkAccess();
+}
+
+$lastActive = \bizley\podium\models\Activity::lastActive();
+?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-<meta charset="<?= Yii::$app->charset ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?= Html::csrfMetaTags() ?>
-<title><?= Html::encode(Helper::title($this->title)) ?></title>
-<?php $this->head() ?>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=<?php echo $font['name']?>:400,400i,700,700i" rel="stylesheet">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode(Helper::title($this->title)) ?></title>
+    <?php $this->head() ?>
+
+    <style>
+        body {
+            font-family: <?php echo $font['name']?>, sans-serif !important;
+        }
+    </style>
 </head>
+
 <body>
 
-<?php $this->beginBody() ?>
-    <div class="container">
-        <?= $this->render('/elements/main/_navbar') ?>
-        <?= $this->render('/elements/main/_breadcrumbs') ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+<div class="eh_top_slider eh_clear_top_slider">
+    <div class="schedule-tab">
+        <div class="tab-list text-center">
+            <h3 class="eh_subtitle eh_subtitle_forum"><?= Html::encode($this->title) ?></h3>
+
+            <p class="eh_top_slider_bigtitle">Start to feel like yourself again.</p>
+            <div class="eh_top_slider_bigtitle_container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <p>
+                                This free, interactive online program has helped thousands of people like you to feel more optimistic and at peace.
+                            </p>
+                            <p>
+                                The program is divided into sessions that you can work through at your own pace, with a private community of other members who are here to support you in your journey.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
-    <?= $this->render('/elements/main/_footer') ?>
+</div>
+
+<?php $this->beginBody() ?>
+
+<?= $this->render('/elements/main/_navbar') ?>
+
+<div class="eh_top_slider_text_box">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <p>
+                    There are <?= $lastActive['count'] ?> guests browsing.
+                    Browse through <?= \bizley\podium\models\Post::getTotalPosts(); ?> posts in <?= \bizley\podium\models\Thread::getTotalThreads(); ?> topics.
+                </p>
+                <p>
+                    Please welcome our newest members:
+                    <?= Client::getMembersLink() ?>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="eh_content_wrapper">
+    <div class="eh_only_middle-content middle-content eh_middle_cont_top_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <?= $this->render('/elements/main/_breadcrumbs') ?>
+                    <?= Alert::widget() ?>
+                    <?= $content ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="eh_footer">
+    <div class="text-footer text-center">
+        <p><?= Yii::t('common', 'Evolution Health is not a healthcare provider and does not provide medical advice, diagnosis, or treatment.
+            If you are currently thinking about or planning to harm yourself or someone else please call 911 or go to
+            the nearest hospital emergency room.') ?> </p>
+    </div>
+    <div class="text-footer text-center">
+        <div class="row">
+            <div class="col-sm-3">
+                <?= HTML::a( Yii::t('common','CONTACT'),'http://www.evolutionhs.com/contact.html',["target"=>"_blank"])?>
+            </div>
+            <div class="col-sm-3">
+                <?= HTML::a(Yii::t('common','ABOUT'),\yii\helpers\Url::to(['/about']))?>
+            </div>
+            <div class="col-sm-3">
+                <?= HTML::a( Yii::t('common','TERMS OF USE'),\yii\helpers\Url::to(['/site/terms']))?>
+            </div>
+            <div class="col-sm-3">
+                <?= HTML::a( Yii::t('common','PRIVACY'),\yii\helpers\Url::to(['/site/privacy']))?>
+            </div>
+        </div>
+    </div>
+    <div class="container text-center">
+        <p>© <?= Yii::t('common', 'Copyright');
+            echo date('Y') ?> <a href="http://www.evolutionhs.com" target="_blank">Evolution Health
+                Systems</a>.
+            <?= Yii::t('common','All Rights Reserved.')?></p>
+    </div>
+</footer>
 <?php $this->endBody() ?>
 
 </body>
+
 </html>
 <?php $this->endPage() ?>
