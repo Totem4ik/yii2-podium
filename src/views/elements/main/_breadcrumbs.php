499916fb64b24a22ec54e08f572c135249f2cc1d
Bohdan <bohdanhan@gmail.com>
2018-05-15 18:33:41 +0300

    add button advanced search

--- src/views/elements/main/_breadcrumbs.php
+++ src/views/elements/main/_breadcrumbs.php
@@ -19,17 +19,17 @@ use yii\widgets\Breadcrumbs;
 <?php if (!isset($this->params['no-search']) || $this->params['no-search'] !== true): ?>
     <div class="col-sm-3">
         <?= Html::beginForm(['forum/search'], 'get'); ?>
-            <div class="form-group">
-                <div class="input-group">
-                    <?= Html::textInput('query', null, ['class' => 'form-control']); ?>
-                    <div class="input-group-btn">
-                        <button type="submit" class="btn btn-default"  aria-expanded="false"><span class="glyphicon glyphicon-search"></span></button>
-                        <!--                        <ul class="dropdown-menu dropdown-menu-right" role="menu">-->
-                        <!--                            <li><a href="--><?//= Url::to(['forum/advanced-search']) ?><!--">--><?//= Yii::t('podium/view', 'Advanced Search Form') ?><!--</a></li>-->
-                        <!--                        </ul>-->
-                    </div>
+        <div class="form-group">
+            <div class="input-group">
+                <?= Html::textInput('query', null, ['class' => 'form-control']); ?>
+                <div class="input-group-btn">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="glyphicon glyphicon-search"></span></button>
+                    <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                        <li><a href="<?= Url::to(['forum/advanced-search']) ?>"><?= Yii::t('podium/view', 'Advanced Search Form') ?></a></li>
+                    </ul>
                 </div>
             </div>
+        </div>
         <?= Html::endForm(); ?>
     </div>
 <?php endif; ?>
