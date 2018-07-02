d8c6a106c48929ef2d259b04d4dba136a35d3bbc
shs1 <vitaliy.k@outsource-to-ukraine.com>
2018-07-02 16:09:16 +0300

    add count members

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -18,13 +18,13 @@ use app\models\Theme;
 use app\components\CheckAccessClient;
 
 PodiumAsset::register($this);
-$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => Url::to(["/uploads/".Client::getFaviconName()])]);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => Url::to(["/uploads/" . Client::getFaviconName()])]);
 $this->beginPage();
 $this->title = Html::encode($this->title = Client::getTitle());
 
 
-$model=Theme::find()->select('font_id')->where(['client_id'=>Yii::$app->site->id])->one();
-$font=Font::findOne($model->font_id);
+$model = Theme::find()->select('font_id')->where(['client_id' => Yii::$app->site->id])->one();
+$font = Font::findOne($model->font_id);
 
 
 $lastActive = \bizley\podium\models\Activity::lastActive();
@@ -34,7 +34,7 @@ $lastActive = \bizley\podium\models\Activity::lastActive();
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=<?php echo $font['name']?>:400,400i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=<?php echo $font['name'] ?>:400,400i,700,700i" rel="stylesheet">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -44,13 +44,13 @@ $lastActive = \bizley\podium\models\Activity::lastActive();
             font-family: <?php echo $font['name']?>, sans-serif !important;
         }
     </style>
-    <?php if(Yii::$app->language=='fr-FR'): ?>
+    <?php if (Yii::$app->language == 'fr-FR'): ?>
         <style>
             .eh_input_btn_in_nav_box:after {
-                content: 'Fermeture la session / 'attr(data-inline);
+                content: 'Fermeture la session / ' attr(data-inline);
             }
         </style>
-    <?php endif;?>
+    <?php endif; ?>
 </head>
 
 <body>
@@ -58,21 +58,21 @@ $lastActive = \bizley\podium\models\Activity::lastActive();
 <div class="eh_top_slider eh_clear_top_slider">
     <div class="schedule-tab">
         <div class="tab-list text-center">
-            <?php if(Yii::$app->session->getFlash('loginMessage')) : ?>
+            <?php if (Yii::$app->session->getFlash('loginMessage')) : ?>
                 <div class="text_without_login"> <?= Yii::$app->session->getFlash('loginMessage') ?></div>
-            <?php endif;?>
-            <h3 class="eh_subtitle eh_subtitle_forum"><?= Yii::t('common', 'Are You Struggling with Depression or Anxiety?');?></h3>
+            <?php endif; ?>
+            <h3 class="eh_subtitle eh_subtitle_forum"><?= Yii::t('common', 'Are You Struggling with Depression or Anxiety?'); ?></h3>
 
-            <p class="eh_top_slider_bigtitle"><?php echo Yii::t('common', 'Start to feel like yourself again.')?></p>
+            <p class="eh_top_slider_bigtitle"><?php echo Yii::t('common', 'Start to feel like yourself again.') ?></p>
             <div class="eh_top_slider_bigtitle_container">
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-12">
                             <p>
-                                <?php echo Yii::t('common', 'This free, interactive online program has helped thousands of people like you to feel more optimistic and at peace.')?>
+                                <?php echo Yii::t('common', 'This free, interactive online program has helped thousands of people like you to feel more optimistic and at peace.') ?>
                             </p>
                             <p>
-                                <?php echo Yii::t('common', 'The program is divided into sessions that you can work through at your own pace, with a private community of other members who are here to support you in your journey.')?>
+                                <?php echo Yii::t('common', 'The program is divided into sessions that you can work through at your own pace, with a private community of other members who are here to support you in your journey.') ?>
                             </p>
                         </div>
                     </div>
@@ -92,17 +92,18 @@ $lastActive = \bizley\podium\models\Activity::lastActive();
         <div class="row">
             <div class="col-sm-12 text-center">
                 <p>
-                    <?php  echo Yii::t('common',  'There are {guest} guests browsing.',
-                        ['guest'=>$lastActive['count']+16
-                        ])?>
-                    <?php $post= \bizley\podium\models\Post::getTotalPosts();
-                    $topic=\bizley\podium\models\Thread::getTotalThreads();
-                    echo Yii::t('common',  'Browse through {post} posts in {topics} threads.',
-                        ['post'=>$post, 'topics'=>$topic
-                        ])?>
+                    <?php echo Yii::t('common', 'There are {guest} guests browsing.',
+                        ['guest' => $lastActive['count'] + 16
+                        ]) ?>
+                    <?php $post = \bizley\podium\models\Post::getTotalPosts();
+                    $topic = \bizley\podium\models\Thread::getTotalThreads();
+                    echo Yii::t('common', 'Browse through {post} posts in {topics} threads.',
+                        ['post' => $post, 'topics' => $topic
+                        ]) ?>
                 </p>
+                <p> <?php echo Yii::t('common', '{users} Members', ['users' => Client::getCountRegistratedUsers()]) ?></p>
                 <p>
-                    <?php echo Yii::t('common', 'Please welcome our newest members:')?>
+                    <?php echo Yii::t('common', 'Please welcome our newest members:') ?>
                     <?= Client::getMembersLink() ?>
                 </p>
             </div>
@@ -135,21 +136,23 @@ $lastActive = \bizley\podium\models\Activity::lastActive();
                 <?= HTML::a(Yii::t('common', 'CONTACT'), 'http://EvolutionHealth.Systems', ["target" => "_blank"]) ?>
             </div>
             <div class="col-sm-3">
-                <?= HTML::a(Yii::t('common','ABOUT'),\yii\helpers\Url::to(['/about']))?>
+                <?= HTML::a(Yii::t('common', 'ABOUT'), \yii\helpers\Url::to(['/about'])) ?>
             </div>
             <div class="col-sm-3">
-                <?= HTML::a( Yii::t('common','TERMS OF USE'),\yii\helpers\Url::to(['/site/terms']))?>
+                <?= HTML::a(Yii::t('common', 'TERMS OF USE'), \yii\helpers\Url::to(['/site/terms'])) ?>
             </div>
             <div class="col-sm-3">
-                <?= HTML::a( Yii::t('common','PRIVACY'),\yii\helpers\Url::to(['/site/privacy']))?>
+                <?= HTML::a(Yii::t('common', 'PRIVACY'), \yii\helpers\Url::to(['/site/privacy'])) ?>
             </div>
         </div>
     </div>
     <div class="container text-center">
         <p>© <?= Yii::t('common', 'Copyright') . ' '; ?>
-            <?php echo (Yii::$app->language=='fr-FR')? ' ': date('Y') ?> <a href="http://www.evolutionhs.com" target="_blank">Evolution Health
-                Systems</a><?php echo (Yii::$app->language=='fr-FR')? ', ':'.'?>
-            <?= Yii::t('common', 'All Rights Reserved.') ?> <?php echo (Yii::$app->language=='fr-FR')? date('Y'):''?></p>
+            <?php echo (Yii::$app->language == 'fr-FR') ? ' ' : date('Y') ?> <a href="http://www.evolutionhs.com"
+                                                                                target="_blank">Evolution Health
+                Systems</a><?php echo (Yii::$app->language == 'fr-FR') ? ', ' : '.' ?>
+            <?= Yii::t('common', 'All Rights Reserved.') ?> <?php echo (Yii::$app->language == 'fr-FR') ? date('Y') : '' ?>
+        </p>
     </div>
 </footer>
 <?php $this->endBody() ?>
